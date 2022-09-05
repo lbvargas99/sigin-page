9797b48296f2be31f3e142a37d99f86f1d8f638b
Leonardo <lbvargas99@gmail.com>
2022-09-04 21:23:01 -0300

    Adição do modo dark

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,5 +1,11 @@
-const chk = document.getElementById('chk');
-
-chk.addEventListener('change', () => {
-    document.body.classList.toggle('dark');
-});
+function changeMode() {
+    document.getElementById('body').className = 'body-dark';
+    document.getElementById('h1').className = 'text-dark';
+    document.getElementById('p').className = 'text-dark';
+    document.getElementById('login').classList.add('input-dark');
+    document.getElementById('password').classList.add('input-dark');
+    document.getElementById('remember').classList.add('text-dark');
+    document.getElementById('darkmode-btn').classList.add('darkmode-btn');
+    document.getElementById('forgot-password').classList.add('light-green');
+    document.getElementById('footer-bg').src='imgs/bg-2.svg'
+};
